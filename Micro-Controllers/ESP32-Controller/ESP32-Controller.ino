34ba24f7e8453fc20ba0a8375974fe4883f0c21b
DeanG <dmag2@kent.ac.uk>
2023-11-04 15:47:57 +0000

    Basic working Version

--- Micro-Controllers/ESP32-Controller/ESP32-Controller.ino
+++ Micro-Controllers/ESP32-Controller/ESP32-Controller.ino
@@ -1,12 +1,81 @@
 #include <ArduinoJson.h>
+#include <WiFiManager.h>
+#include <EEPROM.h>
+
 
 #define RXp2 16
 #define TXp2 17
+
+// define the number of bytes you want to access
+#define EEPROM_SIZE 1
+bool GSMMode = true;
+
+WiFiManager wm;
+WiFiManagerParameter GSMToggleParam; // global param ( for non blocking w params )
+
+
 
 void setup() {
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // initialize EEPROM with predefined size
+  EEPROM.begin(EEPROM_SIZE);
+
+  bool res;
+
+
+  if(EEPROM.read(0) == 1)
+  {
+    GSMMode = true;
+  }else{
+    GSMMode = false;
+  }
+
+
+  // add a custom input field
+  int customFieldLength = 40;
+  char custom_radio_str[300];
+
+  
+  sprintf(custom_radio_str, "<br/><label for='GSMToggle'>Toggle GSM Mode</label><br></br><input type='radio' name='customfieldid' value='true' %s> On<br><input type='radio' name='customfieldid' value='false' %s> Off",
+        (EEPROM.read(0) == 1 ? "checked" : ""),
+        (EEPROM.read(0) == 0 ? "checked" : "")
+  );
+
+  new (&GSMToggleParam) WiFiManagerParameter(custom_radio_str); // custom html input
+  
+  wm.addParameter(&GSMToggleParam);
+  wm.setSaveParamsCallback(saveParamCallback);
+
+  std::vector<const char *> menu = {"wifi","info","param","sep","restart","exit"};
+  wm.setMenu(menu);
+
+  // set dark theme
+  wm.setClass("invert");
+
+
+
+  res = wm.autoConnect("LifeLinkAP","password"); // password protected ap
+
+  
+
+  if(!res) {
+      Serial.println("Failed to connect");
+      //ESP.restart();
+  } 
+  else {
+      //if you get here you have connected to the WiFi    
+      Serial.println("connected...yeey :)");
+  }
+
+}
+
+void toggleGSM(bool state)
+{
+  state ? EEPROM.write(0,1): EEPROM.write(0,0);
+  GSMMode = state; 
 }
 
 bool isStringEmpty(const char *str) 
@@ -59,19 +128,45 @@ void decodeMessage(const char* message)
   }
 }
 
+String getParam(String name){
+  //read parameter from server, for customhmtl input
+  String value;
+  if(wm.server->hasArg(name)) {
+    value = wm.server->arg(name);
+  }
+  return value;
+}
 
 
+void saveParamCallback(){
+  Serial.println("[CALLBACK] saveParamCallback fired");
+  Serial.println("PARAM customfieldid = " + getParam("GSMToggle"));
+  if(strcmp(getParam("customfieldid").c_str(),"true") == 0){
+    EEPROM.write(0,1);
+  }
+  else{
+     EEPROM.write(0,0);
+  }
+  EEPROM.commit();
+}
+
+void GSMBufferLoop()
+{
+   if(Serial2.available()){
+      DynamicJsonDocument jsonSerialMessage(1024);
+      deserializeJson(jsonSerialMessage, Serial2.readString());
+      const char* message = jsonSerialMessage["message"];
+      if(!isStringEmpty(message))
+      {
+        Serial.println(toUpperCase(message)); 
+        decodeMessage(toUpperCase(message));
+      }
+    }
+}
 
 void loop() {
-  if(Serial2.available()){
-    DynamicJsonDocument jsonSerialMessage(1024);
-    deserializeJson(jsonSerialMessage, Serial2.readString());
-    const char* message = jsonSerialMessage["message"];
-    if(!isStringEmpty(message))
-    {
-      Serial.println(toUpperCase(message)); 
-      decodeMessage(toUpperCase(message));
-    }
+  if(GSMMode){
+    GSMBufferLoop();
   }
 
 }
